fe9ab2e6b908473fc0136109473fdb443f20702d
RahmatMulyas <rahmatseru43@gmail.com>
2023-05-09 18:43:23 +0700

    Commit Test

--- components/Layout/ListPegawai/index.js
+++ components/Layout/ListPegawai/index.js
@@ -47,7 +47,7 @@ export const FormDataListPegawai = ({
     setDataIndexPegawai(dataPegawai);
   }, [dataPegawai]);
 
-  //State List Pegawai test
+  //State List Pegawai
   const [DataPegawai, setDataPegawai] = useState([]);
   const [pegawaiMethod, setPegawaiMethod] = useState("");
   const [modalPegawai, setShowModalPegawai] = useState(false);
